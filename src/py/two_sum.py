2e2a80742ca8382eec48c55c5fa87b601297f662
Darren Fu <kotime42@gmail.com>
2019-03-23 23:20:21 -0700

    Update two_sum.py

--- src/py/two_sum.py
+++ src/py/two_sum.py
@@ -3,6 +3,15 @@
 #For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 #Bonus: Can you do this in one pass?
 
+def two_sum(lst, k):
+    seen = set()
+    for num in lst:
+        if k - num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
 from bisect import bisect_left
 
 
