ac51f39d525281acec51c8cdaf53bcc65a510d78
Darren Fu <kotime42@gmail.com>
2019-03-23 23:53:45 -0700

    d2

--- src/py/product.py
+++ src/py/product.py
@@ -4,11 +4,37 @@
 #Follow-up: what if you can't use division?
 
 def product(lst):
+  total_product = 1
   product_list = []
   for i in range(len(lst)):
-    product = 1
-    for j in range(len(lst)):
-      if (j != i):
-        product *= j
-    product_list.append(product)
+    total_product *= lst[i]
+  for j in range(len(lst)):
+    product_list.append(total_product / lst[j])
   return product_list
+
+def productExceptSelf(lst):
+  len = len(lst)
+  left_arr = [1] * len
+  right_arr = [1] * len
+  product_arr = [1] * len
+  for i in range(1, len):
+    left_arr[i] = lst[i-1] * left_arr[i-1]
+  for j in range(len - 2, -1, -1):
+    right_arr[j] = lst[j+1] * right_arr[j+1]
+  for k in range(len):
+    product_arr[k] = left_arr[k] * right_arr[k]
+  return product_arr
+
+def productExceptSelf(nums):
+  leng = len(nums)
+  product_arr = [1] * leng
+  tmp = 1
+  for i in range(leng):
+    product_arr[i] = tmp
+    tmp *= nums[i]
+  tmp = 1
+  for j in range(leng - 1, -1, -1):
+    product_arr[j] *= tmp
+    tmp *= nums[j]
+  return product_arr
+  
